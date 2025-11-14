fd256fc04e06d25f5579964cadecba6237272e84
Klaus Happacher <k.happacher@gmail.com>
2025-11-14 10:49:13 +0100

    fix fkey selection

--- src/main/kotlin/io/github/klahap/pgen/service/DbService.kt
+++ src/main/kotlin/io/github/klahap/pgen/service/DbService.kt
@@ -299,9 +299,9 @@ class DbService(
                 AND tc.table_schema = kcu.table_schema
             JOIN information_schema.constraint_column_usage AS ccu
                 ON tc.constraint_name = ccu.constraint_name
-                AND tc.table_schema = ccu.table_schema
             WHERE tc.constraint_type = 'FOREIGN KEY'
-                AND ${filter.toFilterString(schemaField = "tc.table_schema", tableField = "tc.table_name")};
+                AND ${filter.toFilterString(schemaField = "tc.table_schema", tableField = "tc.table_name")}
+                AND ${filter.toFilterString(schemaField = "ccu.table_schema", tableField = "ccu.table_name")};
             """
         ) { resultSet ->
             val meta = ForeignKeyMetaData(
