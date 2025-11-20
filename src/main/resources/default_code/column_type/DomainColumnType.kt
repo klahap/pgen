36d7465138e046e4e3548429c62bc51da665dee8
Klaus Happacher <k.happacher@gmail.com>
2025-11-20 08:37:18 +0100

    fix domain column type

--- src/main/resources/default_code/column_type/DomainColumnType.kt
+++ src/main/resources/default_code/column_type/DomainColumnType.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.v1.core.ColumnType
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 
 class DomainColumnType<T : Any>(
     kClass: KClass<T>,
@@ -13,9 +14,18 @@ class DomainColumnType<T : Any>(
     private val getter: KProperty1<T, Any>
 
     init {
-        @Suppress("UNCHECKED_CAST")
-        getter = (kClass.memberProperties.singleOrNull() as? KProperty1<T, Any>)
-            ?: throw IllegalArgumentException("${kClass.qualifiedName} must have a single non nullable property")
+        require(kClass.isValue) { "$kClass is not a value class" }
+
+        val paramName = kClass.primaryConstructor
+            .let { it ?: error("$kClass has no primary constructor") }
+            .parameters.singleOrNull()?.name
+            ?: error("$kClass does not have exactly one constructor parameter")
+
+        getter = kClass.memberProperties
+            .singleOrNull { it.name == paramName }
+            .let { it ?: error("property '$paramName' not found in $kClass") }
+            .also { require(!it.returnType.isMarkedNullable) { "property '$paramName' of $kClass is nullable" } }
+            .let { @Suppress("UNCHECKED_CAST") (it as KProperty1<T, Any>) }
     }
 
     override fun sqlType(): String = sqlType
